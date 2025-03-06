6da59f0eb929cf6bddc4aaa380705c42e8e2e5a7
KamanziOlvie <kamanziolvie@gmail.com>
2025-03-06 08:26:52 +0200

    finalize the project

--- yihub-hackathon/vite.config.js
+++ yihub-hackathon/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure it's set to the root path for deployment
-})
+  base: './', // Use this if assets are not loading properly after deployment
+});
